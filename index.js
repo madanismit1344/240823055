c8eab3cf81953dfd7f3759e4628928b70904d22f
madanismit1344 <madanismit1344@gmail.com>
2025-07-15 11:10:27 +0530

    even odd

---
+++ index.js
@@ -0,0 +1,17 @@
+// console.log("patel smit")
+
+// function add(a,b) {
+//     sum=a+b;
+// }
+
+// 1. Check Even or Odd
+// Definition: Determine if a number is even or odd.
+
+const number=7:
+
+if(number % 2 ==0){
+    console.log("Even");
+}
+else{
+    console.log("odd")
+}
